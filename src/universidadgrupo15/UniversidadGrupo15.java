1dd3afabd11014ca7173f1083ea7fb279ff527d1
Enzo Bulacio <72264564+ErCanaro@users.noreply.github.com>
2023-09-12 20:20:44 -0300

    PruebasYJAR
    Se añadieron pruebas de MateriaData, y se cambio el gitignore para no obviar archivos JAR

--- src/universidadgrupo15/UniversidadGrupo15.java
+++ src/universidadgrupo15/UniversidadGrupo15.java
@@ -1,11 +1,11 @@
 
 package universidadgrupo15;
 
-import java.sql.Connection;
 import java.time.LocalDate;
 import universidadgrupo15.accesoADatos.AlumnoData;
-import universidadgrupo15.accesoADatos.Conexion;
+import universidadgrupo15.accesoADatos.MateriaData;
 import universidadgrupo15.entidades.Alumno;
+import universidadgrupo15.entidades.Materia;
 
 
 public class UniversidadGrupo15 {
@@ -16,9 +16,9 @@ public class UniversidadGrupo15 {
         
                 
         //Alumno diegoRivero= new Alumno (40357953, "Rivero", "Diego", LocalDate.of(1999, 1, 1), true);
-        Alumno diegoRivero= new Alumno (5, 40357953, "Quito", "Armando Esteban", LocalDate.of(1999, 1, 1), true);
+//        Alumno diegoRivero= new Alumno (5, 40357953, "Quito", "Armando Esteban", LocalDate.of(1999, 1, 1), true);
         
-        AlumnoData alu = new AlumnoData();
+//        AlumnoData alu = new AlumnoData();
         
 //        alu.agregarAlumno(diegoRivero);
 //       
@@ -27,7 +27,15 @@ public class UniversidadGrupo15 {
 //        alu.modificarAlumno(diegoRivero);
 //        alu.buscarAlumnoPorDni(32458798);
 
-        alu.listarAlumnos();
+//        alu.listarAlumnos();
+
+        MateriaData matedata = new MateriaData();
+        
+        Materia mate = new Materia("Historia", 2, true);
+        
+//        matedata.AgregarMateria(mate);
+        
+        matedata.buscarMateriaPorID(4);
         
     }
     
